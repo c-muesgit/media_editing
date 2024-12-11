9104984e5241574430ae64c1905c6cded21bde3d
c-muesgit <58312946+c-muesgit@users.noreply.github.com>
2024-12-11 09:17:02 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -43,16 +43,24 @@ def get_music_files(input_folder): #makes dictionary of music files in folders a
                         print(f'Skipping file {os.path.basename(file)}')
                         skipped_txt.write(f'{file_path}\n')
 
-                    title = file_info.get('title', [None])[0]
-                    artist = file_info.get('artist', [None])[0]
+                    try:
+                        title = file_info.get('title', [None])[0]
+                        artist = file_info.get('artist', [None])[0]
+                        album = file_info.get('album', [None])[0]
+                    except:
+                        print(f"Missing info for {file_path}")
 
+                    music_files[ID] = {'file_path':file_path}
+
+                    if title:
+                        music_files[ID] = {'title': title}
                     #artist_name = get_artist(file_path)
                     if artist:
-                        music_files[ID] = {'file_path':file_path,'title': title, 'artist': artist}
-                    else:
-                        print(f"no artist found for {file_path}")
-                        music_files[ID] = {'file_path':file_path}
+                        music_files[ID] = {'artist': artist}
     
+                    if album:
+                        music_files[ID] = {'album': album}
+                    
     return music_files
 
 def remove_artist_names(music_files): #removes artists names from beginning of file if it exists 
@@ -82,6 +90,34 @@ def remove_artist_names(music_files): #removes artists names from beginning of f
         removed_artists_txt.write(f'{l}\n')
     
     removed_artists_txt.close()
+
+def remove_album_names(music_files):
+
+    removed_albums = []
+
+    for ID, info in music_files.items():
+        full_path = info['file_path']
+        path_to_file = os.path.dirname(full_path)
+        file_name = os.path.basename(full_path)
+
+        re_album = info['album']
+        re_album = r"^" + re_album + r"\s?-?_?\s?"
+        match = re.search(re_album, file_name)
+        if match:
+            new_file_name = re.sub(re_album, '', file_name) #change file_name to one without pattern
+            new_path = os.path.join(path_to_file, new_file_name)
+            os.rename(full_path,new_path)
+            print(f'"{file_name}" CHANGED TO "{new_file_name}"')
+            removed_albums.append(new_file_name)
+    try:
+        removed_albums_txt = open('removed_artists.txt', 'x')
+    except:
+        removed_albums_txt = open('removed_artists.txt', 'w')
+
+    for l in removed_albums:
+        removed_albums_txt.write(f'{l}\n')
+    
+    removed_albums_txt.close()
 
 def remove_leading_numbers(music_files): #removes leading numbers from songs like "04 - song.flac" if it exists
 
@@ -118,83 +154,6 @@ def remove_leading_numbers(music_files): #removes leading numbers from songs lik
     all_files_txt.close()
 
     return None
-
-#unused functions
-def convert_m3u_to_xml(input_folder): #unused
-
-    m3u_files = []
-    for (path, directories, files) in os.walk(input_folder):
-        for file in files:
-            if file.endswith(".m3u"):
-                m3u = os.path.join(path,file)
-
-                playlist_name = os.path.splitext(os.path.basename(m3u))[0]
-                print(playlist_name)
-                playlist = ET.Element("Item")
-                
-                ET.SubElement(playlist, "Added")
-                ET.SubElement(playlist, "Added").text = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
-                ET.SubElement(playlist, "LockData").text = "false"
-                ET.SubElement(playlist, "LocalTitle").text = playlist_name
-                ET.SubElement(playlist, "RunningTime").text = "366"
-
-                genres = ET.SubElement(playlist, "Genres")
-                ET.SubElement(genres, "Genre").text = "Alternative"
-                ET.SubElement(genres, "Genre").text = "Indie"
-
-                ET.SubElement(playlist, "OwnerUserId").text = "9af58ec9f8c04f008565bd49fead9193"
-
-                playlist_items_element = ET.SubElement(playlist, "PlaylistItems")
-
-                with open(m3u, 'r') as file:
-                    for line in file:
-                        folder = line.split('/')[0]
-                        m3u_title = line.split('/')[1]
-                        pattern = re_pattern1
-                        match = re.search(pattern, m3u_title)
-                        if match:
-                            new_m3u_title = re.sub(pattern, '', m3u_title)
-                        else:
-                            new_m3u_title = m3u_title
-
-                        relative_path = f"{folder}/{new_m3u_title}"
-                        playlist_item = ET.SubElement(playlist_items_element, "PlaylistItem")
-                        ET.SubElement(playlist_item, "Path").text = f"/data/music/{relative_path}"
-
-                ET.SubElement(playlist, "Shares")
-                ET.SubElement(playlist, "PlaylistMediaType").text = "Audio"
-
-                # Write XML to file
-                tree = ET.ElementTree(playlist)
-
-                os.makedirs(os.path.join(input_folder, 'XML Playlists', playlist_name),exist_ok=True)
-                output_folder = os.path.join(input_folder, 'XML Playlists', playlist_name)
-                xml_output_path = os.path.join(output_folder, 'playlist.xml')
-
-                tree.write(xml_output_path, encoding="utf-8", xml_declaration=True)
-                print(f"XML playlist generated at {xml_output_path}")
-                
-        return None
-def get_artist(file_path): #unused
-    
-    try:
-        if file_path.lower().endswith('.mp3'):
-            audio = EasyID3(file_path)
-            return audio.get('artist', [None])[0]
-        elif file_path.lower().endswith('.flac'):
-            audio = FLAC(file_path)
-            return audio.get('artist', [None])[0]
-        elif file_path.lower().endswith('.m4a'):
-            audio = MP4(file_path)
-            return audio.tags.get('\xa9ART', [None])[0]  # MP4 artist tag
-        elif file_path.lower().endswith('.opus'):
-            audio = OggOpus(file_path)
-            return audio.get('artist', [None])[0]
-        elif file_path.lower().endswith('.aac'):
-            audio = AAC.get(file_path) #untested!
-            return audio.get('artist', [None])[0]
-    except Exception as e:
-        print(f"Error reading {file_path}: {e}")
 
 def main():
     #Tk().withdraw()  # Hide the main Tkinter window
---
+++ unused_functions.py
@@ -0,0 +1,93 @@
+import os
+import re
+from functools import reduce
+from datetime import datetime
+
+from tkinter import Tk
+from tkinter.filedialog import askdirectory
+
+import xml.etree.ElementTree as ET
+
+import mutagen
+from mutagen.easyid3 import EasyID3
+from mutagen.flac import FLAC
+from mutagen.mp4 import MP4
+from mutagen.oggopus import OggOpus
+from mutagen.aac import AAC
+
+#unused functions
+def convert_m3u_to_xml(input_folder): #unused
+
+    m3u_files = []
+    for (path, directories, files) in os.walk(input_folder):
+        for file in files:
+            if file.endswith(".m3u"):
+                m3u = os.path.join(path,file)
+
+                playlist_name = os.path.splitext(os.path.basename(m3u))[0]
+                print(playlist_name)
+                playlist = ET.Element("Item")
+                
+                ET.SubElement(playlist, "Added")
+                ET.SubElement(playlist, "Added").text = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+                ET.SubElement(playlist, "LockData").text = "false"
+                ET.SubElement(playlist, "LocalTitle").text = playlist_name
+                ET.SubElement(playlist, "RunningTime").text = "366"
+
+                genres = ET.SubElement(playlist, "Genres")
+                ET.SubElement(genres, "Genre").text = "Alternative"
+                ET.SubElement(genres, "Genre").text = "Indie"
+
+                ET.SubElement(playlist, "OwnerUserId").text = "9af58ec9f8c04f008565bd49fead9193"
+
+                playlist_items_element = ET.SubElement(playlist, "PlaylistItems")
+
+                with open(m3u, 'r') as file:
+                    for line in file:
+                        folder = line.split('/')[0]
+                        m3u_title = line.split('/')[1]
+                        pattern = re_pattern1
+                        match = re.search(pattern, m3u_title)
+                        if match:
+                            new_m3u_title = re.sub(pattern, '', m3u_title)
+                        else:
+                            new_m3u_title = m3u_title
+
+                        relative_path = f"{folder}/{new_m3u_title}"
+                        playlist_item = ET.SubElement(playlist_items_element, "PlaylistItem")
+                        ET.SubElement(playlist_item, "Path").text = f"/data/music/{relative_path}"
+
+                ET.SubElement(playlist, "Shares")
+                ET.SubElement(playlist, "PlaylistMediaType").text = "Audio"
+
+                # Write XML to file
+                tree = ET.ElementTree(playlist)
+
+                os.makedirs(os.path.join(input_folder, 'XML Playlists', playlist_name),exist_ok=True)
+                output_folder = os.path.join(input_folder, 'XML Playlists', playlist_name)
+                xml_output_path = os.path.join(output_folder, 'playlist.xml')
+
+                tree.write(xml_output_path, encoding="utf-8", xml_declaration=True)
+                print(f"XML playlist generated at {xml_output_path}")
+                
+        return None
+def get_artist(file_path): #unused
+    
+    try:
+        if file_path.lower().endswith('.mp3'):
+            audio = EasyID3(file_path)
+            return audio.get('artist', [None])[0]
+        elif file_path.lower().endswith('.flac'):
+            audio = FLAC(file_path)
+            return audio.get('artist', [None])[0]
+        elif file_path.lower().endswith('.m4a'):
+            audio = MP4(file_path)
+            return audio.tags.get('\xa9ART', [None])[0]  # MP4 artist tag
+        elif file_path.lower().endswith('.opus'):
+            audio = OggOpus(file_path)
+            return audio.get('artist', [None])[0]
+        elif file_path.lower().endswith('.aac'):
+            audio = AAC.get(file_path) #untested!
+            return audio.get('artist', [None])[0]
+    except Exception as e:
+        print(f"Error reading {file_path}: {e}")
